42275f0828c5584acd7be4265011bf21782c8ccb
Cristian Atencia <cristianatencia220673@correo.itm.edu.co>
2022-03-30 21:58:40 -0500

    Component compound redy v-2

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -17,3 +17,11 @@ import { ReactElement } from "react";
       increaseBy: (value: number)=> void;
       product: Product;
   }
+
+  export interface ProductCardHOCProps {
+    ({ children, product }: Props) : JSX.Element,
+    Title: ({ title }: {title?: string }) => JSX.Element,
+    Image: ({ img }: { img?: string }) => JSX.Element,
+    Buttons: () => JSX.Element
+    
+  }
